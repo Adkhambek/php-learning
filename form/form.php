ed4111723be46ec7c09e3b01c2e4f51f4279e3bd
Adham <muzaffarov.adham@gmail.com>
2022-06-14 19:36:44 +0500

    form

---
+++ form/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+<form action="postMethod.php" method="post">
+  <label>
+      <p>Username:</p>
+      <input type="text" name="username" required>
+  </label>
+    <label>
+        <p>Email:</p>
+        <input type="email" name="email" required>
+    </label>
+    <br>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+</body>
+</html>
