417b1fa95e2b99dc518f3c795a62ea37a310b80d
Adham <muzaffarov.adham@gmail.com>
2022-06-16 11:03:30 +0500

    fundamentals

--- fundamentals/printf.php
+++ fundamentals/printf.php
@@ -2,4 +2,14 @@
 $first_name = "Adham";
 $last_name = "Muzaffarov";
 
-printf()
+printf("My name is %s and my surname is %s", $first_name, $last_name);
+
+//%b Argument considered an integer; presented as a binary number
+//%c Argument considered an integer; presented as a character corresponding to that ASCII value
+//%d Argument considered an integer; presented as a signed decimal number
+//%f Argument considered a floating-point number; presented as a floating-point number
+//%o Argument considered an integer; presented as an octal number
+//%s Argument considered a string; presented as a string
+//%u Argument considered an integer; presented as an unsigned decimal number
+//%x Argument considered an integer; presented as a lowercase hexadecimal number
+//%X Argument considered an integer; presented as an uppercase hexadecimal number
--- fundamentals/variables.php
+++ fundamentals/variables.php
@@ -10,4 +10,20 @@
 	echo "<br>";
 	echo $var3;
 	echo "<br>";
+	echo $var3[0]."<br>";
 	var_dump($var4);
+
+$num1 = 5;
+$num2 = "5";
+
+echo $num1 + $num2;
+echo var_dump(+$num2);
+echo gettype($num1);
+var_dump(is_integer($num1));
+
+$value1 = "Hello";
+$value2 =& $value1;
+echo $value1,$value2;
+$value2 = "Goodbye";
+echo $value1, $value2;
+
